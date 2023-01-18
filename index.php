1111aa189a1164886392c5952806cb6c78c88d45
Hmz1hb <hamzalachehabe@gmail.com>
2023-01-18 15:45:02 +0100

    Simulateur coupe du monde

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./java.js" defer></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css?v=<?php echo time(); ?>">
+
+    <title>Qatar Word Cup Simulator</title>
+</head>
+
+<body>
+<header role="banner">
+      <nav class="navbar navbar-expand-md navbar-dark">
+        <div class="container">
+          <a class="navbar-brand" href="index.html"><img src="./images/logo.png" alt="FIFA WORD CUP QATAR 2022 LOGO" width="250" style="margin: 2%;"></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">
+            </span>
+          </button>
+          <ul class="navbar-nav ml-auto">
+              <li class="nav-item cta-btn">
+              <a href="#!" class="k btn btn-outline-light main-btn">
+                <span>Start your journey</span>
+                <ion-icon name="compass-outline"></ion-icon>
+              </a>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+    
+  <div class="background">
+       <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card bg-transparent border border-0">
+              <div class="card-body text-light m-5">
+                <h1 class="card-title">Qatar Word Cup Simulator</h1>
+                <p class="card-text">This is a simulator for the FIFA World Cup Qatar 2022. You can simulate the matches and see the results.</p>
+                <a href="simulator.php" class=" btn btn-outline-light main-btn">
+                  <span>Get the ticket</span>
+                  <ion-icon name="log-out-outline"></ion-icon>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+  <main>
+  </main>
+</body>
+
+</html>
